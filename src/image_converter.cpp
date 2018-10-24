2167c34e0bd7460faa4392b0836c45d12a593d0e
Alex Lavriv <alex.lavirv@gmail.com>
2018-10-24 13:05:04 +0300

    send image

--- src/image_converter.cpp
+++ src/image_converter.cpp
@@ -8,75 +8,24 @@
 #include <string>
 #include <ros/package.h>
 
-static const std::string OPENCV_WINDOW = "Image window";
 
-class ImageConverter
-{
-  ros::NodeHandle nh_;
-  image_transport::ImageTransport it_;
-  image_transport::Subscriber image_sub_;
-  image_transport::Publisher image_pub_;
-
-public:
-  ImageConverter()
-    : it_(nh_)
-  {
-    // Subscrive to input video feed and publish output video feed
-    image_sub_ = it_.subscribe("/camera/image_raw", 1,
-      &ImageConverter::imageCb, this);
-    image_pub_ = it_.advertise("/image_converter/output_video", 1);
-
-    cv::namedWindow(OPENCV_WINDOW);
-  }
-
-  ~ImageConverter()
-  {
-    cv::destroyWindow(OPENCV_WINDOW);
-  }
-
-  void imageCb(const sensor_msgs::ImageConstPtr& msg)
-  {
-    cv_bridge::CvImagePtr cv_ptr;
-    try
-    {
-      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-    }
-    catch (cv_bridge::Exception& e)
-    {
-      ROS_ERROR("cv_bridge exception: %s", e.what());
-      return;
-    }
-
-    // Draw an example circle on the video stream
-    if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-      cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
-
-    // Update GUI Window
-    cv::imshow(OPENCV_WINDOW, cv_ptr->image);
-    cv::waitKey(3);
-
-    // Output modified video stream
-    image_pub_.publish(cv_ptr->toImageMsg());
-  }
-};
 
 int main(int argc, char** argv)
-{
-  ros::init(argc, argv, "image_converter");
- // ImageConverter ic;
-  std::cout<<"alex"<<std::endl;
-  std::string image = ros::package::getPath("ROS_ass1") + "/src/colors.jpg";
+   {
+     ros::init(argc, argv, "image_publisher");
+     ros::NodeHandle nh;
+    image_transport::ImageTransport it(nh);
+      std::string imagepath = ros::package::getPath("ROS_ass1") + "/src/colors.jpg";
+    image_transport::Publisher pub = it.advertise("image_publisher", 1);
+    cv::Mat image = cv::imread(imagepath, CV_LOAD_IMAGE_COLOR);
+    cv::waitKey(30);
+    sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
   
-   cv::Mat src = imread( image, cv::IMREAD_COLOR );
-
-   if( src.empty() )
-    {
-        std::cerr<<"Invalid input image\n";
-        std::cerr<<image<<std::endl;
-        
-        return -1;
+    ros::Rate loop_rate(5);
+    while (nh.ok()) {
+      pub.publish(msg);
+      ros::spinOnce();
+      loop_rate.sleep();
     }
-
-  ros::spin();
-  return 0;
 }
+
